d1061088e8384fa56355a0fadce3a1032f6be731
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-07-28 20:56:42 +0200

    es6 class for Color()

--- raytracer/raytracer.js
+++ raytracer/raytracer.js
@@ -19,34 +19,35 @@ var Vector = (function () {
     };
     return Vector;
 }());
-var Color = (function () {
-    function Color(r, g, b) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
+
+// es6 class
+class Color{
+    constructor(r, g, b) {
+        this.r = r
+        this.g = g
+        this.b = b
     }
-    Color.scale = function (k, v) { return new Color(k * v.r, k * v.g, k * v.b); };
-    Color.plus = function (v1, v2) { return new Color(v1.r + v2.r, v1.g + v2.g, v1.b + v2.b); };
-    Color.times = function (v1, v2) { return new Color(v1.r * v2.r, v1.g * v2.g, v1.b * v2.b); };
-    Color.toDrawingColor = function (c) {
-        var legalize = function (d) { return d > 1 ? 1 : d; };
-        return {
-            r: Math.floor(legalize(c.r) * 255),
-            g: Math.floor(legalize(c.g) * 255),
-            b: Math.floor(legalize(c.b) * 255)
-        };
+    static scale(k, v) { return new Color(k * v.r, k * v.g, k * v.b) }
+    static plus(v1, v2) { return new Color(v1.r + v2.r, v1.g + v2.g, v1.b + v2.b) }
+    static times(v1, v2) { return new Color(v1.r * v2.r, v1.g * v2.g, v1.b * v2.b) }
+    static toDrawingColor(c) {
+        var legalize = function (d) { return d > 1 ? 1 : d }
+        let r= Math.floor(legalize(c.r) * 255)
+        let g= Math.floor(legalize(c.g) * 255)
+        let b= Math.floor(legalize(c.b) * 255)
+        return new Color(r,g,b)
     }
-    Color.white=new Color(1,1,1)
-    Color.yellow=new Color(1,1,0)
-    Color.green=new Color(0,1,0)
-    Color.red=new Color(1,0,0)
-    Color.white = new Color(1.0, 1.0, 1.0);
-    Color.grey = new Color(0.5, 0.5, 0.5);
-    Color.black = new Color(0.0, 0.0, 0.0);
-    Color.background = Color.black;
-    Color.defaultColor = Color.black;
-    return Color;
-}());
+    static get white(){ return new Color(1,1,1) }
+    static get yellow(){ return new Color(1,1,0) }
+    static get green(){ return new Color(0,1,0) }
+    static get red(){ return new Color(1,0,0) }
+    
+    static get grey(){ return new Color(0.5, 0.5, 0.5) }
+    static get black(){ return new Color(0.0, 0.0, 0.0) }
+    static get background(){ return this.black }
+    static get defaultColor(){ return this.black }
+}
+
 var Camera = (function () {
     function Camera(pos, lookAt) {
         this.pos = pos;
