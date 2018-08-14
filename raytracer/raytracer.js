a24c80a332bff256416377e3f98d9c6109d90261
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-08-14 10:41:36 +0200

    using .toPrecision to limit the exaggerated precision of the ray cache (now cache is smaller and cheaper, less resource-hungry)

--- raytracer/raytracer.js
+++ raytracer/raytracer.js
@@ -18,7 +18,7 @@ class Vector{
         return new Vector(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x)
     }
     stringify(){
-        return this.x+','+this.y+','+this.z
+        return this.x.toPrecision(4)+','+this.y.toPrecision(4)+','+this.z.toPrecision(3)
     }
 }
 
